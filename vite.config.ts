4015ad938b0de1deb13f67341189368c91551e47
sosa <svyatoiotec.223@gmail.com>
2026-07-01 21:33:52 +0500

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/qazatlas/',
   plugins: [react()],
   server: {
     allowedHosts: ["beula-trinary-sabra.ngrok-free.dev"],
